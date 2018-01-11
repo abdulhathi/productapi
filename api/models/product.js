94fa121af9cf63dde9dbc94c44485eae47fe456b
Hathi <abdulhathi@gmail.com>
2018-01-11 01:45:38 -0800

    1

--- api/models/product.js
+++ api/models/product.js
@@ -6,7 +6,7 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     Name: { type: String, required: true},
     Price: { type: Number, required: true },
-    ProductImage: { type: String, required: true}
+    ProductImage: { type: String, required: false}
 });
 
 module.exports = mongoose.model("Product", productSchema);
